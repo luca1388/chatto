85e4dc4b56a26cfd1c546169a426185d0713c135
Luca Gandini <luca.gandini88@gmail.com>
2019-11-25 01:14:00 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const Colors = require('../Constants/Colors');
+
+const SERVER_PORT = process.env.PORT || 3000;
+const STATIC_DIR = 'dist';
+
+app.use(express.static(STATIC_DIR));
+
+io.on('connection', socket => {
+    console.log("Socket ID: " + socket.id);
+    console.log('New connection');
+    // io.emit('user-connected', null);
+    socket.broadcast.emit('user-connected');
+
+    socket.emit('welcome', {
+        // username: 'Anonymous',
+        color: Colors[parseInt(Math.random() * Colors.length)]
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Disconnect');
+    });
+    socket.on('new-message', message => {
+        console.log(message);
+        socket.broadcast.emit('new-message', message);
+        // io.emit('new-message', message);
+    });
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data);
+    })
+});
+
+app.get('/', (req, res) => 
+    res.send('Hello world')
+);
+
+
+http.listen(SERVER_PORT, () => {
+    console.log('Server running on port ' + SERVER_PORT);
+});
---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from 'react';
+import css from './App.css';
+import io from 'socket.io-client';
+const socket = io('http://192.168.0.13:3000');
+
+const App = props => {
+    const [message, setMessage] = useState('');
+    const [messageList, setMessageList] = useState([]);
+    const [userName, setUserName] = useState('Anonymous');
+    const [color, setColor] = useState('#000');
+    const [userTyping, setUserTyping] = useState('');
+
+    useEffect(() => {
+        socket.on('welcome', data => {
+            setColor(data.color);
+        });
+
+        socket.on('new-message', msg => {
+            console.log(msg);
+            setMessageList(messageList => messageList.concat(msg));
+        });
+
+        socket.on('user-connected', () => {
+            setMessageList(messageList => messageList.concat({
+                text: "New user connected!",
+                color: '#000'
+            }));
+        });
+
+        socket.on('typing', data => {
+            setUserTyping(data.user);
+        });
+        // TODO cleaning listener when unmounting
+    }, []);
+
+    const onMessageChangeHandler = event => {
+        setMessage(event.target.value);
+        socket.emit('typing', {
+            user: userName,
+            typing: true
+        });
+    };
+
+    const onUsernameChangeHandler = event => {
+        setUserName(event.target.value);
+    };
+
+    const onSendHandler = (event) => {
+        event.preventDefault();
+        if (!message.length) {
+            return false;
+        }
+        setMessageList(messageList => messageList.concat({
+            text: message,
+            user: userName,
+            color: color
+        }));
+        socket.emit('new-message', {
+            text: message,
+            user: userName,
+            color: color
+        });
+        setMessage('');
+        setUserTyping('');
+        return false;
+    };
+
+    return (
+        <div className={css.App}>
+            <h1>Chat demo</h1>
+            <p className={css.Username}>Your username: <input type='text' value={userName} onChange={onUsernameChangeHandler} /></p>
+            <ul className={css.Messages} id="messages">{messageList.map((msg, index) => {
+                return <li style={{ color: msg.color }} className={css.Message} key={index}>
+                    {msg.user ? msg.user + ': ' : null}{msg.text}
+                </li>
+            })}
+            </ul>
+            <div>
+                <form className={css.SendButtonContainer} onSubmit={onSendHandler}>
+                    {userTyping ? <div className={css.Typing}>{userTyping} is typing ...</div> : null}
+                    <div className={css.InputWrapper}>
+                        <input className={css.InputMessage} placeholder="Enter your message" onChange={onMessageChangeHandler} type='text' value={message} />
+                    </div>
+                    <button onClick={onSendHandler} className={css.SendButton}>Send</button>
+                </form>
+            </div>
+
+        </div>
+    );
+};
+
+export default App;
