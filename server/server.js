fbd7910ccaf6e0275cae914477af96a8ff9c0c2b
Luca Gandini <luca.gandini88@gmail.com>
2019-12-04 00:57:25 +0100

    Fixed static path

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
 const Colors = require('../Constants/Colors');
 
@@ -38,7 +39,7 @@ io.on('connection', socket => {
 });
 
 app.get('/', (req, res) => 
-    res.send('Hello world')
+    res.sendFile(path.join(__dirname + '/dist/index.html'))
 );
 
 
