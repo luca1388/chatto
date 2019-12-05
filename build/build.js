3a47d59d8eb7655b0bfdbae1ae1b72dd51db287d
Luca Gandini <lgandini@funambol.com>
2019-12-05 10:32:01 +0100

    Fix now script

--- build/build.js
+++ build/build.js
@@ -1,3 +1,3 @@
 const { readFileSync } = require('fs');
 const { join } = require('path');
-const file = readFileSync(join(__dirname, 'dist', '*.js'), 'utf8');
+const file = readFileSync(join(__dirname, '../dist', '*.js'), 'utf8');
