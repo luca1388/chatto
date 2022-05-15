850945a70e2b3e0a5cf386d0fa5bf019a7fc1f1e
Luca Gandini <lgandini@funambol.com>
2022-05-15 22:13:07 +0200

    Fixing file names

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,26 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-const path = require('path');
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const path = require("path");
 
-const outputDirectory = 'dist';
+const outputDirectory = "dist";
 const serverURL = "http://localhost:3000";
 const webpackDevServerPort = 9000;
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'cheap-module-eval-source-map', // build: slow, rebuild: faster, quality: original source
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, outputDirectory)
+  mode: "development",
+  devtool: "cheap-module-eval-source-map", // build: slow, rebuild: faster, quality: original source
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, outputDirectory),
+  },
+  devServer: {
+    host: "0.0.0.0",
+    contentBase: path.join(__dirname, outputDirectory),
+    port: webpackDevServerPort,
+    hot: true,
+    open: true,
+    proxy: {
+      "/api": serverURL,
     },
-    devServer: {
-        contentBase: path.join(__dirname, outputDirectory),
-        port: webpackDevServerPort,
-        hot: true,
-        open: true,
-        proxy: {
-            "/api": serverURL
-        }
-    }
+  },
 });
